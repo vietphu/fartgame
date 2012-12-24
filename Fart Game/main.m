b46e0c82c47bfb089723c13c07ba02fe53ed9f5b
Edwin van Olst <e.vanolst@gmail.com>
2012-12-24 17:27:27 +0100

    Initial Commit

---
+++ Fart Game/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Fart Game
+//
+//  Created by Edwin van Olst on 24/12/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
