f9b932b8ddcad7028d7989631b96e5cbca4b0c84
Edwin van Olst <e.vanolst@gmail.com>
2012-12-28 17:52:36 +0100

    Work under the HUD
    Improved HUD. Removed gas/fart-meters and replaced with bars.

--- Fart Game/HUDLayer.m
+++ Fart Game/HUDLayer.m
@@ -3,10 +3,10 @@
 
 @implementation HUDLayer
 {
-    CCLabelTTF *fartLabel;
+    //CCLabelTTF *fartLabel;
     CCProgressTimer *fartBar;
     CCProgressTimer *gasBar;
-    CCLabelTTF *gasLabel;
+    //CCLabelTTF *gasLabel;
     CCLabelTTF *scoreLabel;
 }
 
@@ -19,12 +19,12 @@
 }
 
 -(void) updateFartLabel:(int)fart {
-    [fartLabel setString:[NSString stringWithFormat:@"Fart: %d",fart]];
+    //[fartLabel setString:[NSString stringWithFormat:@"Fart: %d",fart]];
     [fartBar setPercentage:fart];
 }
 
 -(void) updateGasLabel:(int)gas {
-    [gasLabel setString:[NSString stringWithFormat:@"Gas: %d",gas]];
+    //[gasLabel setString:[NSString stringWithFormat:@"Gas: %d",gas]];
     [gasBar setPercentage:gas];
 }
 
@@ -37,43 +37,38 @@
 {
 	if( (self=[super init]) ) {
         CGSize size = [[CCDirector sharedDirector] winSize];
-        
+
+        // Bars Interface
+        CCSprite *bars = [CCSprite spriteWithFile:@"bars.png"];
+        bars.anchorPoint = ccp(0,0);
+        bars.position = ccp(10,size.height-35);
+        [self addChild:bars];
+
         // Fart bar
-        fartBar = [CCProgressTimer progressWithSprite:[CCSprite spriteWithFile:@"green_health_bar.png"]];
+        fartBar = [CCProgressTimer progressWithSprite:[CCSprite spriteWithFile:@"bar-fart.png"]];
+        fartBar.anchorPoint = ccp(0,0);
         fartBar.type = kCCProgressTimerTypeBar;
         fartBar.midpoint = ccp(0,0);
-        fartBar.position = ccp(60, size.height - 70);
+        fartBar.position = ccp(12, size.height-22);
         fartBar.barChangeRate = ccp(1,0);
-        fartBar.scale = 5;
         fartBar.percentage = 70;
         [self addChild:fartBar];
 
         // Gas bar
-        gasBar = [CCProgressTimer progressWithSprite:[CCSprite spriteWithFile:@"red_health_bar.png"]];
+        gasBar = [CCProgressTimer progressWithSprite:[CCSprite spriteWithFile:@"bar-gas.png"]];
+        gasBar.anchorPoint = ccp(0,0);
         gasBar.type = kCCProgressTimerTypeBar;
         gasBar.midpoint = ccp(0,0);
-        gasBar.position = ccp(60, size.height - 100);
+        gasBar.position = ccp(12, size.height-33);
         gasBar.barChangeRate = ccp(1,0);
-        gasBar.scale = 5;
         gasBar.percentage = 100;
         [self addChild:gasBar];
-
         
         // Score Label
         scoreLabel = [CCLabelTTF labelWithString:@"Score: 0" fontName:@"Marker Felt" fontSize:20];
+        scoreLabel.color = ccc3(0,0,0);
         scoreLabel.position =  ccp( size.width - 50 , size.height - 20 );
 		[self addChild: scoreLabel];
-
-        // Fart Label
-        fartLabel = [CCLabelTTF labelWithString:@"Fart: 100" fontName:@"Marker Felt" fontSize:16];
-        fartLabel.position =  ccp( 30 , size.height - 20 );
-        fartLabel.horizontalAlignment = kCCTextAlignmentLeft;
-		[self addChild: fartLabel];
-
-        // Gas Label
-        gasLabel = [CCLabelTTF labelWithString:@"Gas: 100" fontName:@"Marker Felt" fontSize:16];
-        gasLabel.position =  ccp( 30 , size.height - 40 );
-		[self addChild: gasLabel];
     }
 	return self;
 }
